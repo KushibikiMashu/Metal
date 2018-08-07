854342af4976e8270efdd474d36e54e7608d011f
KushibikiMashu <kushibiki3413@gmail.com>
2018-08-07 17:43:03 +0900

    [develop]Videoコントローラーにtransactionの例外処理を設定

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -14,37 +14,43 @@ class VideosController < ApplicationController
     @group_id = params[:group_id]
 
     # バリデーションをかける
-    if video == nil || @group_id == nil
+    if video == nil || @group_id == "未選択"
       flash[:notice] = "動画またはグループが選択されていません"
       @groupUsers = GroupUser.where(user_id: @current_user)
       render :new
       return
     end
 
-    # videoインスタンスの作成
-    @video = Video.new(
-      user_id: @current_user.id,
-      group_id: @group_id,
-      color: set_random_color
-      )
+    Video.transaction do
+        # videoインスタンスの作成
+        @video = Video.new(
+          user_id: @current_user.id,
+          group_id: @group_id,
+          color: set_random_color
+          )
 
-    if @video.save
-      # TODO mp4以外の動画がアップされた時の対応をする
-      @group_created_at = Group.find_by(id: @group_id).created_at
-      @group_created_at = timestamp_to_number(@group_created_at)
+        @video.save!
 
-      # TODO @group_created_atの名前のディレクトリを作成する
-      @video_path = "/#{@group_created_at}/#{@video.id}.mp4"
-      @video.video_path = @video_path
-      binding.pry
-      File.binwrite("public#{@video_path}", video.read)
+        # ディレクトリを作成
+        @group_created_at = Group.find_by(id: @group_id).created_at
+        @group_created_at = timestamp_to_number(@group_created_at)
+        if !Dir.exist?("public/" + @group_created_at)
+          Dir.mkdir("public/" + @group_created_at)
+        end
 
-      flash[:notice] = "投稿に成功しました"
-      redirect_to("/index")
-    else
-      flash[:notice] = "動画の保存に失敗しました。再度投稿をお願いします"
-      render :new
+        # videoのパスを作成
+        @video_path = "/#{@group_created_at}/#{@video.id}.mp4"
+        @video.video_path = @video_path
+
+        @video.save!
     end
+        # save!に成功した場合の処理
+        File.binwrite("public#{@video_path}", video.read)
+        flash[:notice] = "投稿に成功しました"
+        redirect_to("/index")
+  rescue => e
+        flash[:notice] = "動画の保存に失敗しました。再度投稿をお願いします"
+        render :new
   end
 
   def destroy
